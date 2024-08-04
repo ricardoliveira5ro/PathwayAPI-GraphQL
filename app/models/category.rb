4cc33c8f8efc78a4ae0cb18c4cb6fab8d629b519
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-08-04 22:41:35 +0100

    db seeding

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  has_many :roadmap_categories, dependent: :destroy
+  has_many :roadmaps, through: :roadmap_categories
+
   validates :name, presence: true
 end
--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,3 +1,8 @@
 class Roadmap < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, required: false
+  has_many :roadmap_categories, dependent: :destroy
+  has_many :categories, through: :roadmap_categories
+  has_many :steps, -> { order(:order) }, dependent: :destroy
+
+  validates :title, :description, :categories, presence: true
 end

181e9b2ad4c86e2303b1dbd68ab354185b9beaa1
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-08-06 12:59:56 +0100

    create step in roadmap

--- app/graphql/mutations/roadmap/create_roadmap.rb
+++ app/graphql/mutations/roadmap/create_roadmap.rb
@@ -3,15 +3,21 @@ module Mutations::Roadmap
     argument :title, String, required: true
     argument :description, String, required: true
     argument :category_ids, [ID], required: true
+    argument :steps, [Types::StepInputType], required: false
 
     field :roadmap, Types::RoadmapType, null: true
     field :errors, Types::ValidationErrorsType, null: true
 
-    def resolve(title:, description:, category_ids:)
+    def resolve(title:, description:, category_ids:, steps: [])
       roadmap = Roadmap.new(title: title, description: description)
 
       categories = Category.find(category_ids)
       roadmap.categories << categories
+
+      steps.each do |step_attrs|
+        step = roadmap.steps.build(step_attrs.to_h)
+        step.save!
+      end
 
       if roadmap.save
         { roadmap: roadmap, success: true }
